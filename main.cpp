afac1c32f6737ed8afc93d1537dc2928a974b1cf
Doronin Stanislav <mugisbrows@gmail.com>
2022-06-02 16:21:48 +0300

    style

--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
 #include <QApplication>
 #include <QCommandLineParser>
 #include "version.h"
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setApplicationName("mugi-serial");
     a.setApplicationVersion(VERSION);
+    a.setStyle(QStyleFactory::create("Fusion"));
     MainWindow w;
     w.show();
     return a.exec();
